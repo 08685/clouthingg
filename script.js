90d97aba995a4146c5502daca914b4a02fe4e83e
08685 <anischaala008@gmail.com>
2026-02-24 12:20:23 +0100

    update

--- script.js
+++ script.js
@@ -11,3 +11,18 @@ links.forEach(link => {
         nav.classList.remove("active");
     });
 });
+const mainImg = document.getElementById("MainImg");
+const smallImg = document.getElementsByClassName("small-img");
+
+smallImg[0].onclick = function() {
+    mainImg.src = smallImg[0].src;
+}
+smallImg[1].onclick = function() {
+    mainImg.src = smallImg[1].src;
+}
+smallImg[2].onclick = function() {
+    mainImg.src = smallImg[2].src;
+}
+smallImg[3].onclick = function() {
+    mainImg.src = smallImg[3].src;
+}
